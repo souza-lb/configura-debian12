9bffcc0ca2b4eb0478f74cc2edec43bbccc1b649
souza-lb <162514139+souza-lb@users.noreply.github.com>
2024-09-04 00:21:54 -0300

    Update debian12.sh
    Adição função para cópia arquivo.

--- debian12.sh
+++ debian12.sh
@@ -5,7 +5,7 @@ nome_git="Leonardo Bruno"
 email_git="souzalb@proton.me"
 
 # Definição do arquivo iso.
-arquivo_iso="./iso/debian-12.6.0-amd64-STICK16GB-1.iso"
+arquivo_iso="./iso/debian-12.7.0-amd64-STICK16GB-1.iso"
 pasta_config="./config"
 
 # Definição do log.
@@ -31,8 +31,8 @@ if [ "$timezone_atual" != "America/Sao_Paulo" ]; then
     echo "[ INFO - timezone ok $(date) ]"
 fi
 
-# Função para cópia de arquivos com backup.
-configurar_arquivo(){
+# Função para cópia de arquivos usando sudo e backup.
+cp_arquivo_sudo(){
     local arquivo_backup="$1"
     local arquivo="$2"
     local arquivo_config="$3"
@@ -43,8 +43,8 @@ configurar_arquivo(){
     fi
 }
 
-# Função para cópia de arquivos simples.
-copia_arquivo(){
+# Função para cópia de arquivos.
+cp_arquivo(){
     local arquivo="$1"
     local arquivo_config="$2"
     if [ ! -f "$arquivo" ]; then
@@ -53,19 +53,29 @@ copia_arquivo(){
     fi
 }
 
+# Função para cópia de arquivos com backup.
+cp_arquivo_backup(){
+    local arquivo_backup="$1"
+    local arquivo="$2"
+    local arquivo_config="$3"
+    if [ ! -f "$arquivo_backup" ]; then
+        sudo mv "$arquivo" "$arquivo_backup"
+        sudo cp "$arquivo_config" "$arquivo"
+        echo "[ INFO - $(basename "$arquivo") ok - $(date) ]"
+    fi
+}
+
 # Ajuste touchpad no Xorg.
-configurar_arquivo "/usr/share/X11/xorg.conf.d/40-libinput.conf.backup" "/usr/share/X11/xorg.conf.d/40-libinput.conf" "$pasta_config/40-libinput.conf"
+cp_arquivo_sudo "/usr/share/X11/xorg.conf.d/40-libinput.conf.backup" "/usr/share/X11/xorg.conf.d/40-libinput.conf" "$pasta_config/40-libinput.conf"
 
 # Ajuste no grub.
-configurar_arquivo "/etc/default/grub.backup" "/etc/default/grub" "$pasta_config/grub"
+cp_arquivo_sudo "/etc/default/grub.backup" "/etc/default/grub" "$pasta_config/grub"
 sudo update-grub
 
 # Ajuste no gerenciamento de energia.
-configurar_arquivo "/etc/UPower/UPower.conf.backup" "/etc/UPower/UPower.conf" "$pasta_config/UPower.conf"
-configurar_arquivo "/etc/systemd/logind.conf.backup" "/etc/systemd/logind.conf" "$pasta_config/logind.conf"
-configurar_arquivo "$HOME/.bashrc.backup" "$HOME/.bashrc" "$pasta_config/bashrc"
-sudo chown $(whoami) $HOME/.bashrc
-sudo chown $(whoami) $HOME/.bashrc.backup
+cp_arquivo_sudo "/etc/UPower/UPower.conf.backup" "/etc/UPower/UPower.conf" "$pasta_config/UPower.conf"
+cp_arquivo_sudo "/etc/systemd/logind.conf.backup" "/etc/systemd/logind.conf" "$pasta_config/logind.conf"
+cp_arquivo_backup "$HOME/.bashrc.backup" "$HOME/.bashrc" "$pasta_config/bashrc"
 
 # Instala pacotes e configura o sources.
 sources_list_backup="/etc/apt/sources.list.backup"
@@ -87,7 +97,7 @@ if [ ! -f "$sources_list_backup" ]; then
     sudo umount /media/cdrom
     echo "[ INFO - pacotes iso ok - $(date) ]"
 
-    configurar_arquivo "$sources_list_backup" "$sources_list" "$pasta_config/sources.list"
+    cp_arquivo_sudo "$sources_list_backup" "$sources_list" "$pasta_config/sources.list"
     sudo apt-get update
 
     sudo apt-get install -y jigdo-file dc3dd mednafen
@@ -95,8 +105,8 @@ if [ ! -f "$sources_list_backup" ]; then
 fi
 
 # Configura o lightdm.
-configurar_arquivo "/etc/lightdm/lightdm.conf.backup" "/etc/lightdm/lightdm.conf" "$pasta_config/lightdm.conf"
-configurar_arquivo "/etc/lightdm/lightdm-gtk-greeter.conf.backup" "/etc/lightdm/lightdm-gtk-greeter.conf" "$pasta_config/lightdm-gtk-greeter.conf"
+cp_arquivo_sudo "/etc/lightdm/lightdm.conf.backup" "/etc/lightdm/lightdm.conf" "$pasta_config/lightdm.conf"
+cp_arquivo_sudo "/etc/lightdm/lightdm-gtk-greeter.conf.backup" "/etc/lightdm/lightdm-gtk-greeter.conf" "$pasta_config/lightdm-gtk-greeter.conf"
 
 # Configura o XFCE4.
 whiskermenu="$HOME/.config/xfce4/panel/whiskermenu-1.rc"
@@ -111,7 +121,7 @@ if [ ! -f "$whiskermenu" ]; then
     xfconf-query -c xfwm4 -p /general/theme -s Greybird-dark
     xfconf-query -c xfce4-panel -p /panels/dark-mode -s true
     xfconf-query -c xfce4-panel -p /plugins/plugin-1 -s whiskermenu
-    copia_arquivo "$whiskermenu" "$pasta_config/whiskermenu-1.rc"
+    cp_arquivo "$whiskermenu" "$pasta_config/whiskermenu-1.rc"
     xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/Super_L -s xfce4-popup-whiskermenu -n -t string
     xfce4-panel -r
     xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/libinput_Tapping_Enabled -n -t int -s 1
@@ -119,26 +129,26 @@ if [ ! -f "$whiskermenu" ]; then
 fi
 
 # Configuração editores de terminal.
-copia_arquivo "$HOME/.nanorc" "$pasta_config/nanorc"
+cp_arquivo "$HOME/.nanorc" "$pasta_config/nanorc"
 mkdir -p $HOME/.vim/{arquivos-backup,arquivos-swap,arquivos-undo}
-copia_arquivo "$HOME/.vimrc" "$pasta_config/vimrc"
+cp_arquivo "$HOME/.vimrc" "$pasta_config/vimrc"
 
 # Configuração geany.
 mkdir -p ~/.config/geany/colorschemes/
-copia_arquivo "$HOME/.config/geany/geany.conf" "$pasta_config/geany.conf"
-copia_arquivo "$HOME/.config/geany/colorschemes/one-dark.conf" "$pasta_config/one-dark.conf"
+cp_arquivo "$HOME/.config/geany/geany.conf" "$pasta_config/geany.conf"
+cp_arquivo "$HOME/.config/geany/colorschemes/one-dark.conf" "$pasta_config/one-dark.conf"
 
 # Configuração bluefish.
 mkdir -p ~/.bluefish
-copia_arquivo "$HOME/.bluefish/rcfile-2.0" "$pasta_config/rcfile-2.0"
+cp_arquivo "$HOME/.bluefish/rcfile-2.0" "$pasta_config/rcfile-2.0"
 
 # Configuração zeal.
 mkdir -p ~/.config/Zeal
-copia_arquivo "$HOME/.config/Zeal/Zeal.conf" "$pasta_config/Zeal.conf"
+cp_arquivo "$HOME/.config/Zeal/Zeal.conf" "$pasta_config/Zeal.conf"
 
 # Configuração terminal.
 mkdir -p ~/.config/xfce4/terminal/
-copia_arquivo "$HOME/.config/xfce4/terminal/terminalrc" "$pasta_config/terminalrc"
+cp_arquivo "$HOME/.config/xfce4/terminal/terminalrc" "$pasta_config/terminalrc"
 
 # Configuração git.
 if [ ! -f "$HOME/.gitconfig" ]; then
